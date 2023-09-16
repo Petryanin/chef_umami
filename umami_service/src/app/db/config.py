880117491e106c504d22f1a1cfa8f96a273a9c6a
petryanin <aspetryanin@gmail.com>
2023-09-16 21:20:16 +0200

    feat: init migrations

--- umami_service/src/app/db/config.py
+++ umami_service/src/app/db/config.py
@@ -3,9 +3,7 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from app.db import const
-from app.db.models.base import Base
 
 
 engine = create_async_engine(const.DATABASE_URL)
 AsyncSessionLocal = async_sessionmaker(engine)
-metadata = Base.metadata
